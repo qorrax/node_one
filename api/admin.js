211763cdd6ba0fddad4d721ca015b6cf6f6ae7b9
qorrax <simplecarrier101@gmail.com>
2023-12-06 00:57:18 -0500

    added some codde

--- api/admin.js
+++ api/admin.js
@@ -1,6 +1,6 @@
 
 import express from 'express';
-
+import bcrypt from 'bcrypt';
 import prisma   from  "./lib/index.js"
 const router = express.Router();
 
@@ -18,6 +18,63 @@ router.get("/", async (req, res) => {
     }
 }
 );
+
+
+// CREATE sign up admin
+
+
+
+router.post("/signup", async (req, res) => {
+    const { name, email, password } = req.body;
+    
+    try {
+
+
+        const existingAdmin = await prisma.admin.findUnique({
+            where: {
+                email: email
+            }
+        });
+
+          if (existingAdmin) {
+            return res.status(409).json({
+                 message: "Admin already exists" 
+                });
+
+          }
+
+          // hash password
+
+          const hashedPassword = await bcrypt.hash(password, 10);
+
+            
+          // create admin
+
+          const newAdmin = await prisma.admin.create({
+             data: {
+
+                name: name,
+                email: email,
+                password: hashedPassword,
+                role:"ADMIN"
+             },
+
+          })
+
+            return res.status(201).json({
+                    message: "Admin is created successfully",
+                    admin: newAdmin
+                }
+            );
+
+            } catch (error) {
+            return res.status(500).json({ error: error.message });
+        }
+
+    }
+
+);
+        
 
 
 
